d908dc4735da74e78f2d2776a6c9a649de25a5a5
NatashaMurashev <nmurashev@gmail.com>
2012-06-24 18:32:23 -0700

    deleted db file

--- db/migrate/20120625005955_change_time_to_big_num.rb
+++
@@ -1,9 +0,0 @@
-class ChangeTimeToBigNum < ActiveRecord::Migration
-  def up
-      change_column :events, :time, :bignum
-  end
-
-  def down
-      remove_column :events, :time, :integer
-  end
-end
